c33ca3de71e8c44fbb6aaedbb87e35a61ebfd7c4
itannermahncke <imahncke@olin.edu>
2024-12-15 18:25:15 -0500

    pull

--- pie_waiterbot/fourbar_module.py
+++ pie_waiterbot/fourbar_module.py
@@ -17,7 +17,7 @@ class FourbarModuleNode(Node):
 
         # angle standards
         self.RESET = 1
-        self.EXTENDED = 120
+        self.EXTENDED = 20
 
         # state controls
         # 0: task not started
